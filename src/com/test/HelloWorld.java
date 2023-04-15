3255a4bff0caeb23e0cdb06e40ed7f43ba853bfd
bharathi <bharattii2019@gmail.com>
2023-04-16 02:58:37 +0530

    some changes

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Run java application hello world");
-		System.out.println("git integrated"); 
+		System.out.println("git integrated one"); 
 
 	}
 
